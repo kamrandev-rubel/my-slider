d4271deb849f4ff4b8477b076bddc1ed779698f9
km-rubel <kamranuddin.rubel1@gmail.com>
2022-03-12 18:25:07 +0300

    finish slider

--- app.js
+++ app.js
@@ -7,13 +7,16 @@ const img = [
     "images/download-6.jpeg",
     "images/download-7.jpeg",
     "images/download-8.jpeg"
-];
+]
 
 
 let imgIndex = 0;
+const sliderImg = document.getElementById('slider-img')
 setInterval(() => {
-    if (imgIndex >= img.length) {
-        imgIndex = 0;
+    if (imgIndex >=img.length) {
+        imgIndex = 0
     }
-    imgIndex++;
-}, 1000);
+    const imgURL = img[imgIndex];
+    sliderImg.setAttribute('src', imgURL)
+    imgIndex++
+}, 1000)
